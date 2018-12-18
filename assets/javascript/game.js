e414fe50bbe54f3d2efa238cf0e64d0e9f2ffe7b
Priyesh <91priyesh@gmail.com>
2018-12-17 21:01:01 -0500

    psuedo code for js completed with variables assigned

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,33 @@
 var name = "priyesh"
 console.log(name);
-var five = ["1","2"]
-console.log(five.length)
+
+
+var magicNum = 0;
+
+var cry1Num = 0;
+var cry2Num = 0;
+var cry3Num = 0;
+var cry4Num = 0;
+
+
+var win = 0;
+var losses = 0;
+
+
+//start button to start the game and reset magicNum and cryNum values
+
+
+// assign random value between 19-120 to magicNum
+
+
+// display magicNum value on the HTML page
+
+
+//assign random value between 1-12 to cry1Num, cry2Num, cry3Num, cry4Num
+
+//click funtion for cry1-cry4, when clicked the number value generated should subtract from the magicNum value
+
+
+//assign conditions for when the value hits 0 player wins and counter increases
+
+//assign conditions for when the value hits below 0 player looses counter increases
